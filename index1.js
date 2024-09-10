7ba8b5f57c75c5e9b71298bdc4654b184e8d6ca0
vuvufelix <vuvufelix7@gmail.com>
2024-09-09 22:12:23 -0300

    fundamento 002

---
+++ index1.js
@@ -0,0 +1,12 @@
+// Arrays e objetos
+
+let meta = ['Feliz', 'Aló']
+
+console.log(meta[0] + " programador")
+
+let metas = {
+    valores: "Ler um livro todo o mês inteiro",
+    checked: false
+}
+
+console.log(metas)
