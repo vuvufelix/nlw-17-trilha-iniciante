4482d49a1650ba62c52195c8b8885e074704bd84
vuvufelix <vuvufelix7@gmail.com>
2024-09-09 22:10:58 -0300

    Fundamento 003

---
+++ index2.js
@@ -0,0 +1,34 @@
+// Função
+
+// arroy function
+const criarMeta = () => {}
+
+function criarMetas() {
+
+}
+
+/*
+    OBS: Para acessar um elemento dentro do meu objeto eu faço o seguinte:
+
+    exemplo:  let object = {
+        value: "Vuvu Félix Miguel João",
+        checkout: false,
+        log: (info) => {
+            console.log(info)
+        }
+    }
+
+    object.value = "Não é mas Vuvu Félix Miguel João"
+
+    OBS: Se eu quizer acessar um elemento do array e neste array tiver um objeto eu posso acessar para dentro desse objto fazendo o seguinte
+
+    exemplo: let object = [
+        meta,
+        {
+            value: "Fidelcio Miguel",
+            checkout: false
+        }
+    ]
+
+    object[1].value
+*/
