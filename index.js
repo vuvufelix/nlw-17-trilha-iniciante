4d18e26b8d9d9e85bd6c3a41855b6e57180e9887
vuvufelix <vuvufelix7@gmail.com>
2024-09-09 22:11:55 -0300

    Inicio do fundamento

---
+++ index.js
@@ -0,0 +1,19 @@
+// Olá mundo !
+
+// Variável global
+
+let mensagem = "Olá eu"  
+
+{
+    // Variável local
+
+    const mensagem = "Olá Mundo!"
+    console.log(mensagem)
+}
+console.log(mensagem)
+
+
+// ==========================================================================
+
+// Arrays e objetos
+
